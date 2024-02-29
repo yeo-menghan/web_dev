f154ccd02b16d13859dd9457eeec7bbbb0a0430f
yeo-menghan <yeomenghan1989@gmail.com>
2024-02-29 16:39:05 +0700

    javascript functions and circles

---
+++ odin_javascript/functions/javascript.js
@@ -0,0 +1,15 @@
+function add7(number){
+  return number + 7;
+}
+
+function capitalise(sentence){
+  return sentence.toUpperCase();
+}
+
+let lastLetter = (sentence) => sentence.slice(-1);
+
+let sum = (a, b) => a+b;
+
+console.log(capitalise("hi"));
+console.log(lastLetter("difhaldfadsjflhladf"));
+console.log(sum(1, 6))
